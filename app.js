c5035ac072c7cef9951a76c5d68d4c9d7c34c4b0
SIDDHANT DIXIT <siddhant.dixit23@gmail.com>
2021-11-22 15:51:57 +0530

    Added Error Exception

--- app.js
+++ app.js
@@ -17,20 +17,27 @@ app.listen(process.env.PORT || 80, () => {
 
 
 app.get("/", async (req, res) => {
-  let startDate = new Date();
-  const response = await axios.get('/api/info');
-  let endDate = new Date();
 
-  let mypage = ``;
-  mypage+=`---- START TIME = ${startDate.toISOString()} <br>`;
-  mypage+=`---- END TIME= ${endDate.toISOString()} <br>`;
-  mypage+=`RESPONSE TIME = ${endDate - startDate} <br><br><br>`;
+  try {
+    let startDate = new Date();
+    const response = await axios.get('/api/info');
+    let endDate = new Date();
+  
+    let mypage = ``;
+    mypage+=`---- START TIME = ${startDate.toISOString()} <br>`;
+    mypage+=`---- END TIME= ${endDate.toISOString()} <br>`;
+    mypage+=`RESPONSE TIME = ${endDate - startDate} <br><br><br>`;
+  
+  
+    mypage+=`Path /api/info <br><br>`;
+    mypage+=`Data <br><br>`;
+    mypage+=`${response.data}`;
+    res.send(mypage);
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
 
-
-  mypage+=`Path /api/info <br><br>`;
-  mypage+=`Data <br><br>`;
-  mypage+=`${response.data}`;
-  res.send(mypage);
 });
 
 app.get("/api/info",(req,res)=>{
