ca5d43287a786702412f61a365265b87fa5136c8
SIDDHANT DIXIT <siddhant.dixit23@gmail.com>
2021-11-22 15:47:21 +0530

    Added API TestTime

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const morgan = require("morgan");
+const axios = require("axios");
 require('dotenv').config()
 
 const app = express();
 
 app.use(morgan("dev"));
+
+app.set('json spaces', 2)
 
 app.listen(process.env.PORT || 80, () => {
   console.log(`Listening at.... http://localhost:${process.env.PORT || 80}`);
@@ -13,8 +16,25 @@ app.listen(process.env.PORT || 80, () => {
 
 
 
-app.get("/", (req, res) => {
-  res.send("<h1>OOP Project LinkedList</h1>");
+app.get("/", async (req, res) => {
+  let startDate = new Date();
+  const response = await axios.get('/api/info');
+  let endDate = new Date();
+
+  let mypage = ``;
+  mypage+=`---- START TIME = ${startDate.toISOString()} <br>`;
+  mypage+=`---- END TIME= ${endDate.toISOString()} <br>`;
+  mypage+=`RESPONSE TIME = ${endDate - startDate} <br><br><br>`;
+
+
+  mypage+=`Path /api/info <br><br>`;
+  mypage+=`Data <br><br>`;
+  mypage+=`${response.data}`;
+  res.send(mypage);
+});
+
+app.get("/api/info",(req,res)=>{
+  res.send('OOP Project LinkedList');
 });
 
 app.use((req, res) => {
