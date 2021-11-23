161a8a2f1bc38d49529e86950b725d3e69f75376
SIDDHANT DIXIT <siddhant.dixit23@gmail.com>
2021-11-23 12:38:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,27 +17,27 @@ app.listen(process.env.PORT || 80, () => {
 
 
 app.get("/", async (req, res) => {
-  // console.log(req.headers);
-  res.send(req.headers);
-  // try {
-  //   let startDate = new Date();
-  //   const response = await axios.get('/api/info');
-  //   let endDate = new Date();
+  // res.send(req.headers);
+  let api_url = `${req.headers['x-forwarded-proto']||'http'}://${req.headers.host}/api/info`;
+  try {
+    let startDate = new Date();
+    const response = await axios.get(api_url);
+    let endDate = new Date();
   
-  //   let mypage = ``;
-  //   mypage+=`---- START TIME = ${startDate.toISOString()} <br>`;
-  //   mypage+=`---- END TIME= ${endDate.toISOString()} <br>`;
-  //   mypage+=`RESPONSE TIME = ${endDate - startDate} <br><br><br>`;
+    let mypage = ``;
+    mypage+=`---- START TIME = ${startDate.toISOString()} <br>`;
+    mypage+=`---- END TIME= ${endDate.toISOString()} <br>`;
+    mypage+=`RESPONSE TIME = ${endDate - startDate} <br><br><br>`;
   
   
-  //   mypage+=`Path /api/info <br><br>`;
-  //   mypage+=`Data <br><br>`;
-  //   mypage+=`${response.data}`;
-  //   res.send(mypage);
-  // } catch (error) {
-  //   console.log(error);
-  //   res.send(error);
-  // }
+    mypage+=`Path /api/info <br><br>`;
+    mypage+=`Data <br><br>`;
+    mypage+=`${response.data}`;
+    res.send(mypage);
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
 
 });
 
