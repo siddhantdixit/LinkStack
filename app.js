fd4a4ac916e83cc77aa8dc128806d3bd0981ef26
SIDDHANT DIXIT <siddhant.dixit23@gmail.com>
2021-11-27 18:46:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const morgan = require("morgan");
 const axios = require("axios");
 var _ = require('lodash');
 const { JWT_LOGIN_Secret, LOGIN_MAXAGE } = require("./config");
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
 
 const app = express();
@@ -12,7 +13,7 @@ app.use(morgan("dev"));
 app.use(express.static('public'));
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
-app.use(cookieparser());
+app.use(cookieParser());
 app.set("view engine", "pug");
 app.set("json spaces", 2);
 
