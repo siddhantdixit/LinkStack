65a2ae0ca7ba5fd1667290a4c07c63ed4358ee13
SIDDHANT DIXIT <siddhant.dixit23@gmail.com>
2021-12-01 00:06:43 +0530

    Added ejs

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use(express.static('public'));
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 app.use(cookieParser());
+app.set("view engine", "ejs");
 app.set("view engine", "pug");
 app.set("json spaces", 2);
 
