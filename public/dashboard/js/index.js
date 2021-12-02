44f6bff13caa00b0e382ec5aa404c24678dc6c0b
SIDDHANT DIXIT <siddhant.dixit23@gmail.com>
2021-12-02 23:46:08 +0530

    Fixed URL

--- public/dashboard/js/index.js
+++ public/dashboard/js/index.js
@@ -18,7 +18,7 @@ $('#update_user').submit(function(event){
     console.log(data)
 
     var request={
-        "url":`http://localhost:3000/api/users/${data.id}`,
+        "url":`dashboard/api/users/${data.id}`,
         "method":"PUT",
         "data":data
     }
@@ -34,7 +34,7 @@ if(window.location.pathname=="/"){
         var id=$(this).attr("data-id")
 
         var request={
-            "url":`http://localhost:3000/api/users/${id}`,
+            "url":`/dashboard/api/users/${id}`,
             "method":"DELETE"
         } 
 
