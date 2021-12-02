a6e420e95a37acd8ed552cda4045393c7fa4d289
SIDDHANT DIXIT <siddhant.dixit23@gmail.com>
2021-12-03 00:45:20 +0530

    Removed If from Dashboard Page [BUG] 🪲

--- public/dashboard/js/index.js
+++ public/dashboard/js/index.js
@@ -28,21 +28,19 @@ $('#update_user').submit(function(event){
     })
 })
 
-if(window.location.pathname=="/"){
-    $ondelete=$(".table tbody td a.delete");
-    $ondelete.click(function(){
-        var id=$(this).attr("data-id")
+$ondelete=$(".table tbody td a.delete");
+$ondelete.click(function(){
+    var id=$(this).attr("data-id")
 
-        var request={
-            "url":`/dashboard/api/users/${id}`,
-            "method":"DELETE"
-        } 
+    var request={
+        "url":`/dashboard/api/users/${id}`,
+        "method":"DELETE"
+    } 
 
-        if(confirm("Do you really want to delete this record ?")){
-            $.ajax(request).done(function(response){
-                alert("Data Deleted Successfully!")
-                location.reload()
-            })  
-        }
-    })
-}
+    if(confirm("Do you really want to delete this record ?")){
+        $.ajax(request).done(function(response){
+            alert("Data Deleted Successfully!")
+            location.reload()
+        })  
+    }
+})
