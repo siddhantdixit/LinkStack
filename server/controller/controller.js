7aa84f5595af3e12bd6516eab45a2ed864e1b58b
SIDDHANT DIXIT <siddhant.dixit23@gmail.com>
2021-12-03 16:57:28 +0530

    Remapped Delete API

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -125,13 +125,15 @@ Profile.findOneAndUpdate(
 }
 
 // Delete a new identified user by user id 
-exports.delete=(req,res)=>{
-  const id=req.params.id;
+exports.delete= async (req,res)=>{
+  const linkid=req.params.id;
 
-  Userdb.findByIdAndDelete(id)
+  let myuserprof = await Profile.findOne({userid:res.locals.myuserid});
+  myuserprof.links.id(linkid).remove();
+  myuserprof.save()
   .then(data=>{
       if(!data){
-          res.status(404).send({message:`Cannot delete with id ${id}. Maybe id is wrong`})
+          res.status(404).send({message:`Cannot delete with id ${linkid}. Maybe id is wrong`})
       }else{
          res.send({
              message:"User was deleted successfully!"
@@ -140,7 +142,7 @@ exports.delete=(req,res)=>{
   })  
    .catch(err=>{
        res.status(500).send({
-           message:"Could not delete User with id="+id
+           message:"Could not delete User with id="+linkid
        });
    });
 }
