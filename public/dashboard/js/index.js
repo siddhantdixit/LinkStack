da20c85f4524ba9676467030e7bc06a07ec19315
harshrawat20 <77675473+harshrawat20@users.noreply.github.com>
2021-12-08 13:16:48 +0530

    Profile avatar
    Some issue implementing the avatar

--- public/dashboard/js/index.js
+++ public/dashboard/js/index.js
@@ -44,3 +44,28 @@ $ondelete.click(function(){
         })  
     }
 })
+
+
+// Profile Pic Upload Css
+$(document).ready(function() {
+	
+    var readURL = function(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('.profile-pic').attr('src', e.target.result);
+            }
+    
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+   
+    $(".file-upload").on('change', function(){
+        readURL(this);
+    });
+    
+    $(".upload-button").on('click', function() {
+       $(".file-upload").click();
+    });
+});
